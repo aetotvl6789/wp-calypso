5ab010430224f96a6f82671a007f18f3589dd7fe
aetotvl6789 <nguyenviethuanlove043@gmail.com>
2022-06-25 21:54:41 +0700

    Delete desktop directory

--- desktop/app/window-handlers/debug-tools/index.js
+++
@@ -1,7 +0,0 @@
-const { ipcMain: ipc } = require( 'electron' );
-
-module.exports = function ( { view } ) {
-	ipc.on( 'toggle-dev-tools', function () {
-		view.webContents.toggleDevTools();
-	} );
-};
--- desktop/test/e2e/config/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	cacheDirectory: '<rootDir>/.cache/jest',
-	rootDir: '../../../',
-	testEnvironment: 'node',
-	testMatch: [ '<rootDir>/test/e2e/specs/*.js' ],
-	verbose: true,
-};
