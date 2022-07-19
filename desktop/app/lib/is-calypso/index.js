122f7eee1b7c1e96af4a51ffb50d9c2e2992f227
aetotvl6789 <nguyenviethuanlove043@gmail.com>
2022-07-19 23:09:50 +0700

    Delete desktop directory

--- desktop/app/lib/is-calypso/index.js
+++
@@ -1,7 +0,0 @@
-const Config = require( '../../lib/config' );
-
-const webBase = Config.baseURL();
-
-module.exports = function ( mainWindow ) {
-	return mainWindow.webContents.getURL().startsWith( webBase );
-};
